d36be6b618e31748837e675595632a02dce49150
Jha, Atul Kumar <AtulKumar.Jha@carelon.com>
2023-11-15 22:00:30 +0000

    Update Tailwind config and SignUpForm component

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -13,39 +13,55 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-});
+import { SignupValidation } from "@/lib/validation";
 
 const SignUpForm = () => {
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
     defaultValues: {
+      name: "",
       username: "",
+      email: "",
+      password: "",
     },
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof SignupValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values);
   }
 
   return (
-    <>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/assets/images/logo.svg" alt="Logo" />
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+          Create a new account
+        </h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2">
+          To use SocialGram enter your details
+        </p>
+
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-5 w-full mt-4"
+        >
           <FormField
             control={form.control}
-            name="username"
+            name="name"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Username</FormLabel>
+                <FormLabel>Name</FormLabel>
                 <FormControl>
-                  <Input placeholder="shadcn" {...field} />
+                  <Input
+                    type="text"
+                    className="shad-input"
+                    placeholder="Jack Sparrow"
+                    {...field}
+                  />
                 </FormControl>
                 <FormDescription>
                   This is your public display name.
@@ -54,10 +70,29 @@ const SignUpForm = () => {
               </FormItem>
             )}
           />
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    className="shad-input"
+                    placeholder="jack_sparrow"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>You can change this later.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <Button type="submit">Submit</Button>
         </form>
-      </Form>
-    </>
+      </div>
+    </Form>
   );
 };
 
---
+++ src/lib/validation/index.ts
@@ -0,0 +1,16 @@
+import * as z from "zod";
+
+export const SignupValidation = z.object({
+  name: z
+    .string()
+    .min(2, { message: "Name Too Short" })
+    .max(50, { message: "Name Too Long" }),
+  username: z
+    .string()
+    .min(2, { message: "Username Too Short" })
+    .max(50, { message: "Username Too Long" }),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long" }),
+});
