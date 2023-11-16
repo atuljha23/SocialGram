65ca27e7ec6c035e3e99edbbe54eb226ba6126b9
Jha, Atul Kumar <AtulKumar.Jha@carelon.com>
2023-11-16 19:20:41 +0000

    Add react-dropzone and attr-accept packages, and update PostForm component in CreatePost page

---
+++ src/components/shared/FileUploader.tsx
@@ -0,0 +1,38 @@
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { Button } from "../ui/button";
+
+const FileUploader = () => {
+  const [fileUrl, setFileUrl] = useState("");
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  return (
+    <div
+      className="flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer"
+      {...getRootProps()}
+    >
+      <input className="cursor-pointer" {...getInputProps()} />
+      {fileUrl ? (
+        <div>Test 1</div>
+      ) : (
+        <div className="file_uploader-box">
+          <img
+            src="/assets/icons/file-upload.svg"
+            width={96}
+            height={77}
+            alt="file-upload"
+          />
+          <h3 className="base-medium text-light-2 mb-2 mt-6">
+            Drag photos here
+          </h3>
+          <p className="text-light-4 small-regular mb-6">SVG, PNG, JPG</p>
+          <Button className="shad-button_dark_4">Select from computer</Button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FileUploader;
